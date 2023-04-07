752d2ae3d2f2060efb5a15bcea59164f645522a1
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-07 14:38:12 -0500

    toggle for about it complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [about, setAbout] = useState(null);
+  const [isOpenAbout, setIsOpenAbout] = useState(false);
+  const [isOpenContact, setIsOpenContact] = useState(false);
+  const [isOpenResume, setIsOpenResume] = useState(false); // New state for resume
 
-export default About
+  useEffect(() => {
+    async function getAboutData() {
+      const response = await fetch("./about.json");
+      const data = await response.json();
+      setAbout(data);
+    }
+    getAboutData();
+  }, []);
+
+  const handleToggleAbout = () => {
+    setIsOpenAbout(!isOpenAbout);
+  };
+
+  const handleToggleContact = () => {
+    setIsOpenContact(!isOpenContact);
+  };
+
+  const handleToggleResume = () => {
+    // Function to toggle resume
+    setIsOpenResume(!isOpenResume);
+  };
+
+  const loaded = () => {
+    return (
+      <div>
+        {about.map((desc, index) => (
+          <div key={index}>
+            <p>{desc.image}</p>
+            <p>{desc.name}</p>
+            <p>{desc.title}</p>
+            <div onClick={handleToggleAbout}>
+              <h2>About</h2>
+              {isOpenAbout && <p>{desc.about}</p>}
+            </div>
+            <div onClick={handleToggleResume}>
+              <h2>Resume</h2>
+              {isOpenResume && (
+                <a
+                  className="download-button"
+                  target="_blank"
+                  href="https://drive.google.com/file/d/1J0nQfA7mHHTgftLOruim18z0ADKrGBhI/view?usp=share_link"
+                >
+                  Download Resume
+                </a>
+              )}
+            </div>
+            <div onClick={handleToggleContact}>
+              <h2>Contact</h2>
+              {isOpenContact && (
+                <div>
+                  <p>{desc.linkedIn}</p>
+                  <p>{desc.github}</p>
+                  <p>{desc.email}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return about ? loaded() : <h1>Loading...</h1>;
+};
+
+export default About;
