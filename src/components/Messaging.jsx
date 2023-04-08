85dfc72d20c094c300634204ee1371e3977417a2
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-07 20:29:12 -0500

    output clears when i clear conversation

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 const Messaging = ({ title, description, github, live, convo, images }) => {
   const [inputValue, setInputValue] = useState("");
   const [output, setOutput] = useState("");
-  const [inputValuesList, setInputValuesList] = useState([]); // New state to store entered input values
+  const [inputValuesList, setInputValuesList] = useState(
+    () => JSON.parse(localStorage.getItem("conversation")) || [] // Load conversation from local storage on component mount
+  );
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -12,12 +14,20 @@ const Messaging = ({ title, description, github, live, convo, images }) => {
   const handleButtonClick = () => {
     if (inputValue === "yes") {
       setOutput("Okay! Here they are!");
-    } else {
-      setOutput("");
     }
-    setInputValue(""); // Clear the input value
-    setInputValuesList([...inputValuesList, inputValue]); // Add entered input value to the list
+    setInputValue("");
+    setInputValuesList([...inputValuesList, inputValue]);
   };
+
+  const handleClearConversation = () => {
+    setInputValuesList([]); // Clear input values list
+    localStorage.removeItem("conversation"); // Remove conversation from local storage
+    setOutput(""); // Clear output
+  };
+
+  useEffect(() => {
+    localStorage.setItem("conversation", JSON.stringify(inputValuesList)); // Save conversation to local storage on input values list update
+  }, [inputValuesList]);
 
   return (
     <div className="messaging-container">
@@ -37,19 +47,21 @@ const Messaging = ({ title, description, github, live, convo, images }) => {
         <p>{description}</p>
         <div>
           {output && (
-            <div>              {inputValuesList.map((inputValue, index) => (
-                <div key={index}>{inputValue}</div>
-              ))}
+            <div>        <div>
+          {inputValuesList.map((inputValue, index) => (
+            <div key={index}>{inputValue}</div>
+          ))}
+        </div>
               {output}
               {images.map((pic, index) => (
                 <p key={index} className="">
                   <img src={pic} alt="pic" className="convo-pic" />
                 </p>
               ))}
-
             </div>
           )}
         </div>
+
       </div>
       <div className="mess-in">
         <input
@@ -60,8 +72,8 @@ const Messaging = ({ title, description, github, live, convo, images }) => {
         />
         <button onClick={handleButtonClick}>Submit</button>
       </div>
-      {/* Render the entered input values as div elements */}
-      <div></div>
+      <button onClick={handleClearConversation}>Clear Conversation</button>{" "}
+      {/* Add a button to clear conversation */}
     </div>
   );
 };
