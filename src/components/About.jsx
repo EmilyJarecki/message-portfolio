cf4ea71b6e34acfa10ea229a307fea44a39d2edc
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-12 22:52:15 -0500

    better accordian- collapses when next one opens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,11 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const About = () => {
   const [about, setAbout] = useState(null);
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   useEffect(() => {
     async function getAboutData() {
@@ -28,99 +33,122 @@ const About = () => {
               <p className="desc-title">{desc.title}</p>
             </div>
             <div>
-              <Accordion
-                sx={{
-                  backgroundColor: "transparent",
-                  border: "none",
-                  boxShadow: "none",
-                }}
-              >
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
+              <div>
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}
+                  sx={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    boxShadow: "none",
+                  }}
                 >
-                  <Typography
-                    sx={{ color: "white", fontWeight: "bold", fontWeight: 300 }}
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
                   >
-                    About
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails sx={{ backgroundColor: "transparent" }}>
-                  <Typography sx={{ color: "white", fontWeight: 300 }}>
-                    {desc.about}
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion
-                sx={{
-                  backgroundColor: "transparent",
-                  border: "none",
-                  boxShadow: "none",
-                }}
-              >
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography
-                    sx={{ color: "white", fontWeight: "bold", fontWeight: 300 }}
-                  >
-                    Resume
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails sx={{ backgroundColor: "transparent" }}>
-                  <Typography sx={{ color: "white", fontWeight: 300 }}>
-                    <a
-                      className="download-button"
-                      target="_blank"
-                      href="https://drive.google.com/file/d/1J0nQfA7mHHTgftLOruim18z0ADKrGBhI/view?usp=share_link"
+                    <Typography
+                      sx={{
+                        color: "white",
+                        fontWeight: "bold",
+                        fontWeight: 300,
+                      }}
                     >
-                      Download Resume
-                    </a>
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>{" "}
-              <Accordion
-                sx={{
-                  backgroundColor: "transparent",
-                  border: "none",
-                  boxShadow: "none",
-                }}
-              >
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
+                      About
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ backgroundColor: "transparent" }}>
+                    <Typography sx={{ color: "white", fontWeight: 300 }}>
+                      {desc.about}
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}
+                  sx={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    boxShadow: "none",
+                  }}
                 >
-                  <Typography
-                    sx={{ color: "white", fontWeight: "bold", fontWeight: 300 }}
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
                   >
-                    Contact
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails sx={{ backgroundColor: "transparent" }}>
-                  <Typography sx={{ color: "white", fontWeight: 300 }}>
-                    <p>
-                      <a className="" href={desc.linkedIn} target="_blank">
-                        LinkedIn
+                    <Typography
+                      sx={{
+                        color: "white",
+                        fontWeight: "bold",
+                        fontWeight: 300,
+                      }}
+                    >
+                      Resume
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ backgroundColor: "transparent" }}>
+                    <Typography sx={{ color: "white", fontWeight: 300 }}>
+                      <a
+                        className="download-button"
+                        target="_blank"
+                        href="https://drive.google.com/file/d/1J0nQfA7mHHTgftLOruim18z0ADKrGBhI/view?usp=share_link"
+                      >
+                        Download Resume
                       </a>
-                    </p>
-                    <p>
-                      {" "}
-                      <a className="" href={desc.github} target="_blank">
-                        GitHub
-                      </a>
-                    </p>
-                    <p>
-                      <a className="email" href="mailto:jareckiemily@yahoo.com">
-                        jareckiemily@yahoo.com
-                      </a>
-                    </p>
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel3"}
+                  onChange={handleChange("panel3")}
+                  sx={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    boxShadow: "none",
+                  }}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                  >
+                    <Typography
+                      sx={{
+                        color: "white",
+                        fontWeight: "bold",
+                        fontWeight: 300,
+                      }}
+                    >
+                      Contact
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ backgroundColor: "transparent" }}>
+                    <Typography sx={{ color: "white", fontWeight: 300 }}>
+                      <p>
+                        <a className="" href={desc.linkedIn} target="_blank">
+                          LinkedIn
+                        </a>
+                      </p>
+                      <p>
+                        {" "}
+                        <a className="" href={desc.github} target="_blank">
+                          GitHub
+                        </a>
+                      </p>
+                      <p>
+                        <a
+                          className="email"
+                          href="mailto:jareckiemily@yahoo.com"
+                        >
+                          jareckiemily@yahoo.com
+                        </a>
+                      </p>
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
             </div>
           </div>
         ))}
@@ -139,5 +167,4 @@ const About = () => {
     </div>
   );
 };
-
 export default About;
